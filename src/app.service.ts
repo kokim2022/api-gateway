0572062d600ef77ddd1f6d9cc0a8bb9ad442069f
hwaung <102525421+kokim2022@users.noreply.github.com>
2023-11-02 15:52:32 +0630

    client initialized

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { createOrderRequest } from './create-order-request.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createOrder({ userId, price }: createOrderRequest) {
+    console.log(userId);
+    console.log(price);
   }
 }
---
+++ src/create-order-request.dto.ts
@@ -0,0 +1,5 @@
+export class createOrderRequest {
+  userId: string;
+
+  price: number;
+}
